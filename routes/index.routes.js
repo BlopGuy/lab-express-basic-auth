eebedd882793266ccb9fd8fc71ab53641692880c
BlopGuy <gvieira.assuntos@outlook.pt>
2021-02-11 11:12:05 +0000

    3rd iteration complete

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,6 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index', {user: req.session.currentUser}));
+router.get('/', (req, res, next) => res.render('index', { user: req.session.currentUser }));
+
+function requireLogin(req, res, next) {
+    if (req.session.currentUser) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
+router.get('/private', requireLogin, (req, res) => {
+    res.render('private');
+});
+
+router.get('/main', requireLogin, (req, res) => {
+    res.render('main');
+});
 
 module.exports = router;
